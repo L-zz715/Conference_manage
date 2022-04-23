12982243a71ce2e5b456c17cfd7847d7c0857247
Lucy_Zhou <Lu_Zhou05@hotmail.com>
2022-04-24 00:35:47 +1000

    elementui css

--- conference_management/src/pages/Home.vue
+++ conference_management/src/pages/Home.vue
@@ -1,26 +1,30 @@
 <template>
   <div>
     <h3>Home</h3>
-      <el-button type="info" round @click="logout">log out</el-button>
+    <el-button type="info" round @click="logout">log out</el-button>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-    name:'Home',
-    data(){
-      return{
-
-      }
+  name: "Home",
+  data() {
+    return {};
+  },
+  methods: {
+    logout() {
+      this.$router.push("login");
     },
-    methods:{
-      logout(){
-        this.$router.push('login')
-      }
-    }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
