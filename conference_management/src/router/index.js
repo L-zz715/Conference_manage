4e54960e7388cea7d894bbaa50ae66cfdf70976d
Lucy_Zhou <Lu_Zhou05@hotmail.com>
2022-04-24 13:02:07 +1000

    add user.vue in router

--- conference_management/src/router/index.js
+++ conference_management/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/pages/Login'
 import Home from '@/pages/Home'
+import Users from '@/pages/user/Users'
 
 Vue.use(VueRouter)
 
@@ -21,7 +22,14 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: Home,
-
+    children:[
+      {
+        path: '/users',
+        name: 'Users',
+        component: Users,
+    
+      },
+    ]
   },
 
 
