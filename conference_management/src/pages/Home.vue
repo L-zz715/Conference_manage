c0cfb691e9e6328f2249c0125ab88e2130cfeaa5
Lucy_Zhou <Lu_Zhou05@hotmail.com>
2022-04-24 12:44:21 +1000

    modify aside list

--- conference_management/src/pages/Home.vue
+++ conference_management/src/pages/Home.vue
@@ -21,29 +21,25 @@
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-message"></i>
-              导航一
+              用户管理
             </template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-            <el-menu-item index="1-3">选项3</el-menu-item>
+            <el-menu-item index="1-1">用户列表</el-menu-item>
+            <el-menu-item index="1-2">权限管理</el-menu-item>
           </el-submenu>
           <el-submenu index="2">
             <template slot="title">
               <i class="el-icon-menu"></i>
-              导航二
+              会议管理
             </template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-menu-item index="2-1">会议列表</el-menu-item>
           </el-submenu>
           <el-submenu index="3">
             <template slot="title">
               <i class="el-icon-menu"></i>
-              导航san
+              文章管理
             </template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-menu-item index="3-1">文章列表</el-menu-item>
+            <el-menu-item index="3-2">审核</el-menu-item>
           </el-submenu>
         </el-menu>
       </el-aside>
