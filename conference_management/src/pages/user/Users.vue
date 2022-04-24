781b122781ad1e9d8875e857a0beb04713f5128b
Lucy_Zhou <Lu_Zhou05@hotmail.com>
2022-04-24 12:54:53 +1000

    create simple user

---
+++ conference_management/src/pages/user/Users.vue
@@ -0,0 +1,21 @@
+<template>
+  <h3>User</h3>
+</template>
+
+<script>
+export default {
+    name:'Users',
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
