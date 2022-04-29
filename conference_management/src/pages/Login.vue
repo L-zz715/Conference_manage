d7fa73aee505bc651c290d1fa75b1458a312a333
Lucy_Zhou <Lu_Zhou05@hotmail.com>
2022-04-29 18:15:18 +1000

    create login form

--- conference_management/src/pages/Login.vue
+++ conference_management/src/pages/Login.vue
@@ -1,25 +1,142 @@
 <template>
-  <div>
+  <div class="login_container">
+    <div class="login_box">
       <h3>Login</h3>
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="25%"
+        class="login_form"
+      >
+        <el-form-item label="用户：" prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="el-icon-user-solid"
+          >
+          </el-input>
+        </el-form-item>
+        <el-form-item label="密码：" prop="password">
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
       <el-button type="primary" round @click="loginToHome">login</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Login",
-  data(){
-    return{
-
-    }
+  data() {
+    return {
+      loginForm: {
+        username: "admin",
+        password: "123456",
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入用户名称", trigger: "blur" },
+          {
+            min: 1,
+            max: 15,
+            message: "长度在 1 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 4,
+            max: 16,
+            message: "长度在 4 到 16 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
   },
-  methods:{
-    loginToHome(){
-      this.$router.push('home')
-    }
-  }
+  methods: {
+    login() {
+      this.$refs.loginFormRef.validate(async (valid) => {
+        if (!valid) return;
+        // console.log(this)
+
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        console.log("sf", res);
+        if (res.meta.status !== 200) {
+          return this.$message.error("登录失败");
+        }
+
+        this.$message.success("登录成功");
+
+        //保存token
+        window.sessionStorage.setItem("token", res.data.token);
+
+        this.$router.push("/home");
+      });
+    },
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields();
+    },
+  },
 };
 </script>
 
-<style>
+<style lang="less" scoped>
+.login_container {
+  height: 100%;
+  background-color: #2b4b6b;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    //less  的语法嵌套
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    overflow: hidden;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background: #fff;
+
+    img {
+      width: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.btns {
+  // float: right;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.login_form {
+  width: 90%;
+  position: absolute;
+  bottom: 0;
+}
 </style>
