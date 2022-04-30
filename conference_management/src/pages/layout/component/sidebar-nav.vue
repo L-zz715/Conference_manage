5ac6f9f42dfbfeea46fe03b3967f0de2329bcf8b
Lucy_Zhou <Lu_Zhou05@hotmail.com>
2022-04-30 21:19:20 +1000

    add dynamic menu test

--- conference_management/src/pages/layout/component/sidebar-nav.vue
+++ conference_management/src/pages/layout/component/sidebar-nav.vue
@@ -1,16 +1,16 @@
 <template>
+  <!--   :collapse="isCollapse"
+      :collapse-transition="false" -->
   <el-aside width="200px">
     <el-menu
       background-color="#2441a3"
       text-color="#fff"
       active-text-color="#409EFF"
       :unique-opened="true"
-      :collapse="isCollapse"
-      :collapse-transition="false"
       :router="true"
       :default-active="activePath"
     >
-      <el-submenu index="1">
+      <!-- <el-submenu index="1">
         <template slot="title">
           <i class="el-icon-message"></i>
           用户管理
@@ -38,18 +38,24 @@
         </template>
         <el-menu-item index="papers">文章列表</el-menu-item>
         <el-menu-item index="review">审核</el-menu-item>
-      </el-submenu>
+      </el-submenu> -->
+      <DynamicMenu :menuList="sidebarMenu"></DynamicMenu>
     </el-menu>
   </el-aside>
 </template>
 
 <script>
+import DynamicMenu from "@/components/dynamic-menu";
+
 export default {
   data() {
     return {
       isCollapse: false,
       activePath: "",
     };
+  },
+  components: {
+    DynamicMenu,
   },
 };
 </script>
