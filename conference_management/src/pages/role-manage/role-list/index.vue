2f91cfefd92409421bf5dee7b097e9067bf31d29
Lucy_Zhou <Lu_Zhou05@hotmail.com>
2022-05-06 15:00:03 +1000

    create rolelist structure

--- conference_management/src/pages/role-manage/role-list/index.vue
+++ conference_management/src/pages/role-manage/role-list/index.vue
@@ -1,20 +1,32 @@
 <template>
   <div>
-      角色列表
+    <!-- 面包屑导航 -->
+    <Breadcrumb />
+
+    <!-- 用户列表区 -->
+    <el-table :data="roleList" style="width: 100%" border stripe>
+      <!-- 下面这一个column添加的是索引链 -->
+      <el-table-column type="index"> </el-table-column>
+      <el-table-column prop="rolename" label="角色名称"> </el-table-column>
+      <el-table-column prop="description" label="角色描述"> </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
+import Breadcrumb from "@/components/Breadcrumb.vue";
+import Pagination from "@/components/Pagination.vue";
 export default {
-    data(){
-        return{}
-    },
-    mounted(){
-        
-    }
-}
+  name: "Roles-list",
+  components: { Breadcrumb, Pagination },
+  data() {
+    return {
+      roleList: [],
+    };
+  },
+  mounted() {},
+};
 </script>
 
 <style>
-
 </style>
