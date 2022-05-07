6c5f4283b966813bd2a4241ec475c22c00538b40
Lucy_Zhou <Lu_Zhou05@hotmail.com>
2022-05-07 19:44:12 +0800

    test

--- conference_management/src/pages/conference-manage/conference-list/index.vue
+++ conference_management/src/pages/conference-manage/conference-list/index.vue
@@ -504,9 +504,17 @@ export default {
           this.usernameList.push(item.username);
         }
       });
-
-      console.log(confer);
       this.editForm = confer;
+      const dt = new Date(this.editForm.date);
+      // 解决时差问题
+      dt.setMinutes(dt.getMinutes() + dt.getTimezoneOffset());
+      const y = dt.getFullYear();
+      const m = (dt.getMonth() + 1 + "").padStart(2, "0");
+      const d = (dt.getDate() + "").padStart(2, "0");
+      const hh = (dt.getHours() + "").padStart(2, "0");
+      const mm = (dt.getMinutes() + "").padStart(2, "0");
+      const ss = (dt.getSeconds() + "").padStart(2, "0");
+      this.editForm.date = `${y}-${m}-${d} ${hh}:${mm}:${ss}`;
       this.editDialogVisible = true;
     },
   },
